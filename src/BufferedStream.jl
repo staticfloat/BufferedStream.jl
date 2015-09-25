1346f2a1605dcfb119e88c94fd70b51b795b49d7
Elliot Saba <staticfloat@gmail.com>
2015-09-24 19:32:16 -0700

    It's working.... IT'S WORKING!

--- src/BufferedStream.jl
+++ src/BufferedStream.jl
@@ -1,161 +1,158 @@
 module BufferedStream
 
-import Base: getindex, length, push!, eltype, linearindexing, size, similar, finalizer
-export BufferedStreamView, IndexedBufferedStream
-export shift_origin!, view!
+import Base: getindex, length, push!, eltype, linearindexing, size, similar, show
+export LinkedStream, StreamView
+export shift_origin!
 
-# BufferedStream is an abstraction of a data stream that flows, but remembers
-# as much of the stream as views/iterators upon it force it to.
-abstract AbstractBufferedStream{T}
+# The foundation of a linked-list of chunks
+type StreamChunk{T}
+    data::Array{T,1}
+    next::Nullable{StreamChunk{T}}
 
-# A view that keeps track of where it is in a BufferedStream
-type BufferedStreamView{T} <: DenseArray{T, 1}
-    stream::AbstractBufferedStream{T}
+    StreamChunk() = new(T[], Nullable{StreamChunk{T}}())
+    StreamChunk(data::Array{T,1}) = new(data, Nullable{StreamChunk{T}}())
+end
 
+# Just return the length of this chunk.
+length(chunk::StreamChunk) = length(chunk.data)
+
+# Show something well-behaved
+function show(io::IO, chunk::StreamChunk)
+    write(io, "StreamChunk")
+    Base.showlimited(io, chunk.data)
+    if isnull(chunk.next)
+        write(io, " -> <null>")
+    else
+        write(io, " -> StreamChunk")
+    end
+end
+
+# The object that keeps track of the last chunk in the linked list
+type LinkedStream{T}
+    # This is where our lovely loving data gets stored
+    latest::StreamChunk{T}
+
+    # How many samples have we discarded?
+    samples_past::Int64
+
+    # We can only create one that initializes with an empty list
+    LinkedStream() = new(StreamChunk{T}(), 0)
+end
+
+# Pushes a new chunk onto the end of the stream.  Julia should automatically garbage collect
+# the chunks any views have moved past, since they will no longer be accessible.
+function push!{T}(stream::LinkedStream{T}, data::Array{T,1})
+    # Create a new chunk based off of this data
+    chunk = StreamChunk{T}(data)
+
+    # Account for the samples we're putting behind us
+    stream.samples_past += length(stream.latest)
+
+    # Move the new chunk into the lead position
+    stream.latest.next = Nullable(chunk)
+    stream.latest = chunk
+    return
+end
+
+function show{T}(io::IO, stream::LinkedStream{T})
+    write(io, "LinkedStream{$(string(T))}, $(stream.samples_past) samples post-origin")
+end
+
+
+
+
+# A view that acts like an array, but that you can move forward in the stream
+type StreamView{T} <: DenseArray{T, 1}
     # Where is our origin?
-    chunk_idx::Int64
+    chunk::StreamChunk{T}
     sample_offset::Int64
 
     # What is our origin's absolute index?
     abs_idx::Int64
 end
 
+# Show something well-behaved
+#function show(io::IO, view::StreamView)
+#    Base.show_vector(io, view, "StreamView[", "]")
+#end
+
 # Define stuff for AbstractArray
-linearindexing(::BufferedStreamView) = Base.LinearFast()
+linearindexing(::StreamView) = Base.LinearFast()
 # Calculates how many samples after our origin there are buffered up
-length(view::BufferedStreamView) = max(sum(Int64[length(z) for z in view.stream.data_chunks[view.chunk_idx:end]]) - view.sample_offset, 0)
-size(view::BufferedStreamView) = (length(view),)
-similar(view::BufferedStreamView, T, dims::Tuple{Int64}) = T[z for z in view]
-eltype{T}(view::BufferedStreamView{T}) = T
+function length(view::StreamView)
+    # Count total length of all chunks after the origin of this guy
+    len = length(view.chunk)
+    chunk = view.chunk
+    while !isnull(chunk.next)
+        chunk = chunk.next.value
+        len += length(chunk)
+    end
 
-# Construct a new view off of a stream
-function BufferedStreamView{T}(stream::AbstractBufferedStream{T}, offset::Integer = 0)
-    view = BufferedStreamView{T}(stream, 1, 0, stream.samples_discarded)
+    # Subtract sample_offset, and ensure that zero is the smallest we will ever return
+    return max(len - view.sample_offset, 0)
+end
+
+size(view::StreamView) = (length(view),)
+eltype{T}(view::StreamView{T}) = T
+
+
+
+
+# Construct a new view off of a stream, with an optional offset
+function StreamView{T}(stream::LinkedStream{T}, offset::Int64 = 0)
+    view = StreamView{T}(stream.latest, 0, stream.samples_past)
     shift_origin!(view, offset)
-    push!(stream.views, WeakRef(view))
     return view
 end
 
-# Construct a new view off of another view
-function BufferedStreamView{T}(v::BufferedStreamView{T})
-    view = BufferedStreamView{T}(v.stream, v.chunk_idx, v.sample_offset, v.stream.samples_discarded)
-    push!(v.stream.views, WeakRef(view))
+# Construct a new view off of another view (easy peasey; just a straight copy)
+StreamView{T}(v::StreamView{T}) = StreamView{T}(v.chunk, v.sample_offset, v.abs_idx)
 
-    # When a view dies, take its weakref with it
-    finalizer(view, (view) -> begin
-        idx = 1
-        while idx <= length(view.stream.views)
-            if view.stream.views[idx] == view
-                splice!(view.stream.views, idx)
-            else
-                idx += 1
-            end
-        end
-    end)
-    return view
-end
+# Move this view forward `offset` samples; we only support positive offsets!
+function shift_origin!(view::StreamView, offset::Integer)
+    if offset < 0
+        throw(DomainError("Only positive shifts allowed!"))
+    end
 
-# Move this view forward `offset` samples
-function shift_origin!(view::BufferedStreamView, offset::Integer)
     # Bump up the absolute index immediately
     view.abs_idx += offset
 
-    # Figure out how many chunks we need to move forward
-    for chunk in view.stream.data_chunks
-        if offset >= length(chunk) - view.sample_offset
-            # This means we skip this chunk entirely
-            offset -= length(chunk) - view.sample_offset
-            view.chunk_idx += 1
-            view.sample_offset = 0
-        else
-            # This means we land somewhere within this chunk
-            view.sample_offset += offset
-            clean!(view.stream)
-            return
-        end
+    # Move through chunks until offset lands within this current chunk, or we run out of chunks
+    while (offset >= length(view.chunk) - view.sample_offset) && !isnull(view.chunk.next)
+        offset -= length(view.chunk) - view.sample_offset
+        # sample_offset can be greater than length(view.chunk), if we had shifted past the end of
+        # buffered data previously; this will move us forward as far as we can
+        view.sample_offset = max(view.sample_offset - length(view.chunk), 0)
+        view.chunk = view.chunk.next.value
     end
 
-    # We have passed the end of the data we've received; chunk_idx  is set to
-    # the index of the next chunk that will be created, so set sample_offset to the
-    # remaining offset that we haven't gobbled up with the
-    view.sample_offset = offset
-    clean!(view.stream)
+    # Once our offset lands within this chunk, update the sample_offset and call it good!  If we are
+    # past the end of the current offset (e.g. we are shifting into the great unknown) do the same,
+    # simply leaving sample_offset greater than length(view.chunk)
+    view.sample_offset += offset
     return
 end
 
-
 # If someone asks for an index, serve it if we can!
-function getindex(view::BufferedStreamView, idx::Integer)
-    # Calculate which chunk to read from:
-    curr_idx = idx + view.sample_offset
-    for chunk in view.stream.data_chunks[view.chunk_idx:end]
-        # Is the current offset contained within this chunk?  If so, return it!  Otherwise, continue
-        if curr_idx <= length(chunk)
-            return chunk[curr_idx]
-        else
-            curr_idx -= length(chunk)
+function getindex(view::StreamView, idx::Integer)
+    # Scoot us forward as much as the sample_offset requires
+    idx += view.sample_offset
+
+    # Zoom through chunks as much as we need
+    chunk = view.chunk
+    while idx > length(chunk)
+        # If we can't move forward anymore, throw a BoundsError!
+        if isnull(chunk.next)
+            throw(BoundsError())
         end
+        idx -= length(chunk)
+        chunk = chunk.next.value
     end
-    throw(BoundsError())
+
+    # If we got to the correct chunk, index into it and yield the data!
+    return chunk.data[idx]
 end
 
 # Don't define setindex!() for now; that may or may not be a great idea.
-
-
-
-
-
-type IndexedBufferedStream{T} <: AbstractBufferedStream{T}
-    # Keep track of the views out in the world
-    views::Array{WeakRef}
-
-    # This is where our lovely loving data gets stored
-    data_chunks::Array{Array{T}}
-
-    # How many chunks have we discarded?
-    chunks_discarded::Int64
-
-    # How many samples have we discarded?
-    samples_discarded::Int64
-
-    IndexedBufferedStream() = new(BufferedStreamView[], Array[], 0, 0)
-end
-
-# The length is how much stuff is buffered up right now, total
-length(stream::IndexedBufferedStream) = sum([length(z) for z in stream.data_chunks])
-
-# Pushes a new chunk onto the end of the stream.  Note that if there are no viewers for this stream
-# registered, we just throw the bits away immediately.
-function push!{T}(stream::IndexedBufferedStream{T}, chunk::Array{T})
-    if isempty(stream.views)
-        stream.chunks_discarded += 1
-        stream.samples_discarded += length(chunk)
-    else
-        push!(stream.data_chunks, chunk)
-    end
-    return
-end
-
-# Remove any chunks that aren't being pointed to by views anymore
-function clean!(stream::IndexedBufferedStream)
-    # Find the first chunk in use by any of the views
-    min_chunk = length(stream.data_chunks)
-    for view in stream.views
-        min_chunk = min(view.value.chunk_idx, min_chunk)
-    end
-
-    # If min_chunk is > 1, we have work to do; chunks to free!
-    if min_chunk > 1
-        # Free the chunks in one swell foop
-        stream.data_chunks = stream.data_chunks[min_chunk:end]
-        stream.chunks_discarded += min_chunk - 1
-
-        # Update the views so they are still pointing to the proper chunks
-        for view in stream.views
-            view.value.chunk_idx -= min_chunk - 1
-        end
-    end
-    return
-end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,74 +1,98 @@
 using BufferedStream
 using Base.Test
 
-# Test that things throw with negatives, test that things get thrown away, etc...
-stream = IndexedBufferedStream{Float64}()
+# Construct a stream and a view of said stream
+stream = LinkedStream{Float64}()
+view = StreamView(stream)
 
-# First, test that samples pushed when there are no viewers are thrown away
-@test length(stream) == 0
-@test length(stream.views) == 0
-push!(stream, [1.0,2.0,3.0])
-@test length(stream) == 0
-
-# Next, test that if we register a view, we can start buffering samples
-view = BufferedStreamView(stream)
-@test length(stream.views) == 1
-push!(stream, [1.0,2.0,3.0])
-@test length(stream) == 3
-@test length(view) == 3
-push!(stream, [4.0,5.0,6.0])
-@test length(stream) == 6
-@test length(view) == 6
-
-# Test that indexing is on the up and up
-for idx in 1:length(stream)
-    @test view[idx] == Float64(idx)
-end
-
-# Ensure that we can throw out-of-bounds stuffage
-@test_throws BoundsError view[0]
-@test_throws BoundsError view[7]
+# Initially, any kind of access on view is going to throw
 @test_throws BoundsError view[-1]
+@test_throws BoundsError view[0]
+@test_throws BoundsError view[1]
+@test view.chunk == stream.latest
+@test isnull(view.chunk.next)
 
-# Test cloning a view
-view2 = BufferedStreamView(view)
-@test length(stream.views) == 2
-for idx in 1:length(stream)
-    @test view2[idx] == view[idx]
-    @test view2[idx] == Float64(idx)
+# Add a chunk of data
+data = Float64[3.1,4.1,5.9,2.6,5.3]
+push!(stream, data)
+@test view.chunk != stream.latest
+@test view.chunk.next.value == stream.latest
+@test isnull(stream.latest.next)
+
+# Print out a StreamChunk and a StreamView to see what it looks like:
+display(stream)
+println()
+display(view)
+println()
+
+# Ensure that the data came through okay, and that out of bounds accesses are recognized as such
+@test length(view) == length(data)
+for idx in 1:length(data)
+    @test view[idx] == data[idx]
+end
+@test_throws BoundsError view[-1]
+@test_throws BoundsError view[0]
+@test_throws BoundsError view[6]
+
+# Verify that the LinkedStream does not copy data:
+data[1] = 3.2
+@test view[1] == 3.2
+data[1] = 3.1
+@test view[1] == 3.1
+
+# Add a second chunk, ensure that it is seamlessly integrated into our view
+push!(stream, data)
+for idx in 1:length(data)
+    @test view[idx] == data[idx]
+    @test view[idx + length(data)] == data[idx]
 end
 
-# Test moving one of the iterators
-shift_origin!(view2, 4)
-@test view2.chunk_idx == 2
-@test view2.sample_offset == 1
-@test length(view) == 6
-@test length(view2) == 2
-@test view[5] == view2[1]
-@test view[6] == view2[2]
-@test_throws BoundsError view2[3]
+# Move view forward a few samples, ensure the shift worked:
+shift_origin!(view, 2)
+@test length(view) == 2*length(data) - 2
+for idx in 1:length(view)
+    @test view[idx] == data[mod1(idx + 2, length(data))]
+end
 
-# Now move another iterator, this one moves the views such that the first chunk is freeable
-shift_origin!(view, 3)
-@test length(stream.data_chunks) == 1
-@test length(stream) == 3
-@test view[2] == view2[1]
-@test view[3] == view2[2]
+# Test that the underlying data is still the original representations
+@test view.chunk.data == data
 
-# Prove that the view's chunk indices have been updated after making the transition above
-@test view.chunk_idx == 1
-@test view2.chunk_idx == 1
+# Move view forward enough to get into the next chunk
+shift_origin!(view, 4)
+@test length(view) == length(data) - 1
+for idx in 1:length(view)
+    @test view[idx] == data[idx + 1]
+end
 
-# Let's make sure we can use things like sum(), mean(), etc...
-@test sum(view) == 15
-@test mean(view) == 5.0
+# Now add a bunch of chunks
+push!(stream, Float64[1:100 ...])
+push!(stream, Float64[1:100 ...])
+push!(stream, Float64[1:100 ...])
+push!(stream, Float64[1:100 ...])
 
-# Let's test out having lots of weirdly shaped chunks and iterating over them
-push!(stream, map(Float64, [1:7...]))
-push!(stream, map(Float64, [1:57...]))
-push!(stream, [1.0])
-push!(stream, [1.0])
-push!(stream, [1.0])
-@test length(stream) == 70
+# Ensure our length can still be found, despite all appearances to the contrary
+@test length(view) == 404
 
-@test view[1:10] == Float64[1,2,3,4,5,6,7,1,2,3]
+# Shift the view, and start playing with more complex operations/indexing
+shift_origin!(view, 157)
+@test view[48:147] == Float64[1:100 ...]
+@test length(view[48:end]) == 200
+@test mean(view[48:end]) == mean(1:100)
+@test sum(view[48:57]) == sum(1:10)
+
+# Create a second view off of this view, and a third view off of the stream
+view2 = StreamView(view)
+view3 = StreamView(stream)
+
+# Test to make sure that the linked list looks like we'd expect
+@test view3.sample_offset == 0
+@test view2.sample_offset == view.sample_offset
+@test view2.chunk == view.chunk
+@test view2.chunk.next.value.next.value == view3.chunk
+@test isnull(view3.chunk.next)
+
+# Test to make sure the values look like we'd expect
+@test view2.abs_idx == view.abs_idx
+@test view3.abs_idx == view.abs_idx + 48 + 99
+@test view2[1] == view[1]
+@test view3[1] == view[148]
